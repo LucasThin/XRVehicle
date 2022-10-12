fe862543792b194444a1791ed0bc0128368629d4
Nan Phyu Phyu Thin <107634089+LucasThin@users.noreply.github.com>
2022-10-12 13:53:02 -0700

    Merge Trigger Test

--- Assets/PlaneTrigger.cs
+++ Assets/PlaneTrigger.cs
@@ -1,22 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlaneTrigger : MonoBehaviour {
 
-    public GameObject ObjectToEnable;
+    public GameObject planeTrigger;
+    public GameObject planeModel;
+    private bool ifCollision;
+    private bool ifPlaneMode;
 
     void Start () {
-        ObjectToEnable.SetActive (false);        
+        planeTrigger.SetActive (false);
+        gameObject.tag = "Collision";
     }
 
-    void OnTriggerEnter(Collider other) { 
-        ObjectToEnable.SetActive (true);
-    
+    private void Update()
+    {
+        /* if (IfCollision == true)
+        {
+            planeModel.SetActive(true);
+        }
+        else
+        {
+            planeModel.SetActive(false);
+        }*/
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Collision"))
+        {
+            planeTrigger.SetActive (true);
+            ifCollision = true;
+            Debug.Log("Collision Triggered!");
+        }
+        if (other.CompareTag("PlaneTrigger"))
+        {
+            planeModel.SetActive (true);
+            ifPlaneMode = true;
+            Debug.Log("Plane Mode Triggered!");
+        }
+
+
+
     }
 
     void OnTriggerExit(Collider other) {
-        ObjectToEnable.SetActive (false);
+        planeTrigger.SetActive (false);
+        ifCollision = true;
 
     }
 
