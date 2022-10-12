51c83c934570a9c9b31ce7255b42b1173230125d
Khkuu <77972589+Khkuu@users.noreply.github.com>
2022-10-11 23:02:08 -0700

    Complete CarMode 10/22

--- Assets/CarMode.cs
+++ Assets/CarMode.cs
@@ -8,33 +8,48 @@ public class CarMode : MonoBehaviour
 
     public GameObject Test;
     public GameObject CarModeTrigger;
-    public GameObject Camera;
-    public GameObject Height;
+    public GameObject MainCamera;
+    public GameObject CarCamera;
     bool IfCarMode;
 
     //public float CarModeTriggerHeight;
-    public Vector3 cameraOffset = new Vector3 (0f, -0.9f, 0f);
+    //public Vector3 cameraOffset = new Vector3 (0f, -0.9f, 0f);
 
     void Awake()
     {
-        Height.transform.position = new Vector3 (0f, 0f, 0f);
+        MainCamera.SetActive(true);
+        CarCamera.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        CarModeTrigger.transform.position = new Vector3(Camera.transform.position.x, 0f, Camera.transform.position.z);
+        CarModeTrigger.transform.position = new Vector3(MainCamera.transform.position.x, 0.6f, MainCamera.transform.position.z);
+        if (IfCarMode == true)
+        {
+            MainCamera.SetActive(false);
+            CarCamera.SetActive(true);
+        }
+        else
+        {
+            MainCamera.SetActive(true);
+            CarCamera.SetActive(false);
+        }
+
 
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        IfCarMode = true;
         Test.SetActive(true);
-        Height.transform.position = cameraOffset;
+        Debug.Log("Triggered!");
     }
 
     private void OnTriggerExit(Collider other)
     {
+        IfCarMode = false;
         Test.SetActive(false);
+        Debug.Log("Left");
     }
 }
