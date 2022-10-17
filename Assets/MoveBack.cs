04118dcd7acc7fadf343c0d9d060b5b142ba3664
LucasThin <107634089+LucasThin@users.noreply.github.com>
2022-10-17 10:27:44 -0700

    Finish Camera Forward/Backward

--- Assets/MoveBack.cs
+++ Assets/MoveBack.cs
@@ -28,17 +28,17 @@ public class MoveBack : MonoBehaviour
         previousRotation = transform.rotation.y;
         currentX = transform.position.x;
         currentZ = transform.position.z;
+        //Debug.Log(transform.position);
 
-
-        if ( 1 <= previousRotation &&  previousRotation <= 179)
+        /* if ( 0 <= previousRotation &&  previousRotation <= 180)
         {
-            if (currentX - lastX > 0)
+            if (transform.forward.x>0 && transform.forward.z>0)
             {
                 frontCamera.SetActive(true);
                 backCamera.SetActive(false);
                 Debug.Log("Go forward");
             }
-            else
+            else if (transform.forward.x<0 && transform.forward.z<0)
             {
                 frontCamera.SetActive(false);
                 backCamera.SetActive(true);
@@ -59,7 +59,81 @@ public class MoveBack : MonoBehaviour
                 backCamera.SetActive(true);
                 Debug.Log("Go backward");
             }
+        }*/
+        
+        if (transform.forward.x>0 && transform.forward.z>0)
+        {
+            if (currentX - lastX > 0 && currentZ - lastZ > 0)
+            {
+                frontCamera.SetActive(true);
+                backCamera.SetActive(false);
+                Debug.Log("Go forward:0-90");
+            }
+            else if (currentX - lastX < 0 && currentZ - lastZ < 0)
+            {
+                frontCamera.SetActive(false);
+                backCamera.SetActive(true);
+                Debug.Log("Go backward:0-90");
+            }
         }
+        else if (transform.forward.x>0 && transform.forward.z<0)
+        {
+            if (currentX - lastX > 0 && currentZ - lastZ < 0)
+            {
+                frontCamera.SetActive(true);
+                backCamera.SetActive(false);
+                Debug.Log("Go forward:90-180");
+            }
+            else if (currentX - lastX < 0 && currentZ - lastZ > 0)
+            {
+                frontCamera.SetActive(false);
+                backCamera.SetActive(true);
+                Debug.Log("Go backward:90-180");
+            }
+        }
+        else if (transform.forward.x<0 && transform.forward.z<0)
+        {
+            if (currentX - lastX < 0 && currentZ - lastZ < 0)
+            {
+                frontCamera.SetActive(true);
+                backCamera.SetActive(false);
+                Debug.Log("Go forward:-90-0");
+            }
+            else if (currentX - lastX > 0 && currentZ - lastZ > 0)
+            {
+                frontCamera.SetActive(false);
+                backCamera.SetActive(true);
+                Debug.Log("Go backward:-90-0");
+            }
+        }
+        else if (transform.forward.x<0 && transform.forward.z>0)
+        {
+            if (currentX - lastX < 0 && currentZ - lastZ > 0)
+            {
+                frontCamera.SetActive(true);
+                backCamera.SetActive(false);
+                Debug.Log("Go forward:-180 - -90");
+            }
+            else if (currentX - lastX > 0 && currentZ - lastZ < 0)
+            {
+                frontCamera.SetActive(false);
+                backCamera.SetActive(true);
+                Debug.Log("Go backward:-180 - -90");
+            }
+        }
+        
+        /*if (transform.forward.x>0 && transform.forward.z>0)
+        {
+            frontCamera.SetActive(true);
+            backCamera.SetActive(false);
+            Debug.Log("Go forward");
+        }
+        else if (transform.forward.x<0 && transform.forward.z<0)
+        {
+            frontCamera.SetActive(false);
+            backCamera.SetActive(true);
+            Debug.Log("Go backward");
+        }*/
 
         lastX = currentX;
         lastZ = currentZ;
