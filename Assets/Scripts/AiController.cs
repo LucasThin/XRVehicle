1bf6048fc1a3a7e32f563467e3d57ecaf1074dd1
Khkuu <77972589+Khkuu@users.noreply.github.com>
2022-10-18 23:32:19 -0700

    Complete LineRender

--- Assets/Scripts/AiController.cs
+++ Assets/Scripts/AiController.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(LineRenderer))]
+[RequireComponent(typeof(NavMeshAgent))]
+
 public class AiController : MonoBehaviour
 {
     public NavMeshAgent agent;
+    private LineRenderer myLineRender;
 
     [Range(0, 100)] public float speed;
     [Range(1, 500)] public float walkRadius;
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -17,6 +22,13 @@ public class AiController : MonoBehaviour
             agent.speed = speed;
             agent.SetDestination(RandomNavMeshLocation());
         }
+
+        //Line
+        myLineRender = GetComponent<LineRenderer>();
+        myLineRender.startWidth = 0.15f;
+        myLineRender.endWidth = 0.15f;
+        myLineRender.positionCount = 0;
+
     }
 
     // Update is called once per frame
@@ -26,12 +38,13 @@ public class AiController : MonoBehaviour
         {
             agent.SetDestination(RandomNavMeshLocation());
         }
+        DrawPath();
     }
     
     public Vector3 RandomNavMeshLocation()
     {
-        Vector3 finalPosition = Vector3.zero;
         Vector3 randomPosition = Random.insideUnitSphere * walkRadius;
+        Vector3 finalPosition = Vector3.zero;
         randomPosition += transform.position;
         if (NavMesh.SamplePosition(randomPosition, out NavMeshHit hit, walkRadius, 1))
         {
@@ -41,5 +54,20 @@ public class AiController : MonoBehaviour
         return finalPosition;
     }
 
+    void DrawPath()
+    {
+        myLineRender.positionCount = agent.path.corners.Length;
+        myLineRender.SetPosition(0, transform.position);
 
+        if (agent.path.corners.Length < 2)
+        {
+            return;
+        }
+
+        for (int i =1; i < agent.path.corners.Length; i++)
+        {
+            Vector3 pointPosition = new Vector3(agent.path.corners[i].x, agent.path.corners[i].y, agent.path.corners[i].z);
+            myLineRender.SetPosition(i, pointPosition);
+        }
+    }
 }
